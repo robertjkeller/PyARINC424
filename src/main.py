6979a8172233a7ec4163540b5d6772d9c90d289f
Robert <robertjkeller@gmail.com>
2022-03-10 18:22:36 -0500

    Deleted docstrings--too cluttered. Renamed main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ configs = UserConfigs()
 
 
 class ArincRecord:
-    '''Represents an ARINC-424 format file record.'''
     def __init__(self, record_map: dict):
         self.section = record_map.get("section_code")
         self.subsection = record_map.get("subsection_code")
@@ -22,11 +21,6 @@ class ArincRecord:
 
 
 class ArincParser:
-    '''
-    Parser that iterates through record mappings, extracts each record's
-    data from the main ARINC-format file, and adds the data to the 
-    PostgreSQL database.
-    '''
     def __init__(self, cursor, file=configs.file_loc):
         self.cursor = cursor
         self.file = file
@@ -34,7 +28,6 @@ class ArincParser:
         self.cycle = self.get_cycle()
 
     def read_file(self) -> str:
-        '''Reads the CIFP/ARINC-424 formatted file.'''
         with open(self.file) as file:
             return file.readlines()
 
@@ -44,16 +37,13 @@ class ArincParser:
             self.create_arinc_record(record)
 
     def get_cycle(self) -> str:
-        '''Gets the file's AIRAC cycle for schema naming.'''
         return self.lines[0][35:39]
 
     def create_schema(self) -> None:
-        '''Creates database schema for the given cycle. Drops existing schema.'''
         sql = f"DROP SCHEMA IF EXISTS cycle{self.cycle} CASCADE; CREATE SCHEMA cycle{self.cycle};"
         self.cursor.execute(sql)
 
     def create_table(self, record: ArincRecord, cycle: str) -> None:
-        '''Creates a table in the PostgreSQL database for an ARINC record.'''
         sql = f"""
                DROP TABLE IF EXISTS cycle{cycle}.{record.name}; 
                CREATE TABLE cycle{cycle}.{record.name} 
@@ -62,17 +52,12 @@ class ArincParser:
         self.cursor.execute(sql)
 
     def add_row(self, name: str, values: list, cycle: str) -> None:
-        '''Adds a row to its ARINC record PostgreSQL table.'''
         values = [v.replace("'", "''") for v in values]
         values_joined = ", ".join([f"'{v.rstrip()}'" for v in values])
         sql = f"INSERT INTO cycle{cycle}.{name} VALUES ({values_joined});"
         self.cursor.execute(sql)
 
     def create_arinc_record(self, record_map) -> None:
-        '''
-        For a given ARINC record, parse data out of the main file and 
-        add it to the record's PostgreSQL database table.
-        '''
         record = ArincRecord(record_map)
 
         self.create_table(record, self.cycle)
