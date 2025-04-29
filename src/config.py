2b0205e6822961560a97407025d33b3a7e8c2b21
Robert Keller <robertjkeller@gmail.com>
2025-04-28 20:28:53 -0400

    Add config validation

--- src/config.py
+++ src/config.py
@@ -1,26 +1,50 @@
 import configparser
 
+
 class UserConfigs:
     def __init__(self):
         parser = configparser.ConfigParser()
-        parser.read('config.ini')
+        parser.read("config.ini")
+        validate(parser)
 
-        if not parser.has_section('postgres') and not parser.has_section('sqlite'):
-            raise ValueError("No database configuration found in config.ini")
+        if parser.has_section("postgres"):
+            self.dbtype = "postgres"
+            self.dbname = parser["postgres"]["dbname"]
+            self.user = parser["postgres"]["user"]
+            self.password = parser["postgres"]["password"]
+            self.host = parser["postgres"]["host"]
+            self.port = parser["postgres"]["port"]
 
-        if parser.has_section('postgres'):
-            self.dbtype = 'postgres'
-            self.dbname = parser['postgres']['dbname']
-            self.user = parser['postgres']['user']
-            self.password = parser['postgres']['password']
-            self.host = parser['postgres']['host']
-            self.port = parser['postgres']['port']
+        if parser.has_section("sqlite"):
+            self.dbtype = "sqlite"
+            self.dbname = parser["sqlite"]["dbname"]
 
-        if parser.has_section('sqlite'):
-            self.dbtype = 'sqlite'
-            self.dbname = parser['sqlite']['dbname']
-
-        if not parser.has_section('cifp_file'):
+        if not parser.has_section("cifp_file"):
             raise ValueError("No cifp_file configuration found in config.ini")
 
-        self.file_loc = parser['cifp_file']['file_loc']
+        self.file_loc = parser["cifp_file"]["file_loc"]
+
+
+def validate(parser: configparser.ConfigParser) -> None:
+    if not parser.has_section("postgres") and not parser.has_section("sqlite"):
+        raise ValueError("No database configuration found in config.ini")
+
+    if parser.has_section("postgres") and parser.has_section("sqlite"):
+        raise ValueError("Only one database configuration allowed in config.ini")
+
+    if parser.has_section("postgres"):
+        if not all(
+            key in parser["postgres"]
+            for key in ["dbname", "user", "password", "host", "port"]
+        ):
+            raise ValueError("Missing required PostgreSQL configuration keys")
+
+    if parser.has_section("sqlite"):
+        if "dbname" not in parser["sqlite"]:
+            raise ValueError("Missing required SQLite configuration key: dbname")
+
+    if not parser.has_section("cifp_file"):
+        raise ValueError("Missing required cifp_file configuration key: file_loc")
+
+    if not parser.has_option("cifp_file", "file_loc"):
+        raise ValueError("File location cannot be empty")
